e8e7e9db4b83685d4877581628114982c0b32f5f
Ben Stokes <ben@benstokes.org.uk>
2024-12-07 18:57:45 +0000

    simpler is actually faster, also remove brute force code, it's in the commit history

--- haskell/Day7.hs
+++ haskell/Day7.hs
@@ -48,53 +48,20 @@ work_backwards ops least =
     let step n = HashSet.foldl' (add_predecessors n) HashSet.empty in
     foldr step . HashSet.singleton
 
-type Op = Word -> Word -> Word
-
-cat_words :: Op
-cat_words x 0 = x
-cat_words x y =
-    let (yh, yt) = y `quotRem` 10 in
-    10 * cat_words x yh + yt
-
-can_solve :: [Op] -> [InverseOp] -> Equation -> Bool
-can_solve ops inverse_ops equation =
-    let go slow [] =
-          let from_end = work_backwards inverse_ops
-                         (head $ operands equation) (result equation) slow in
-          any (flip HashSet.member from_end)
-        go (x:xs) fast =
-          let add_successors n set prev =
-                foldl (\s op -> let out = op prev n in
-                       if out <= result equation
-                       then HashSet.insert out s
-                       else s) set ops in
-          go xs (drop 2 fast) . HashSet.foldl' (add_successors x) HashSet.empty
-        go [] _ = error "This shouldn't be possible" in
+can_solve :: [InverseOp] -> Equation -> Bool
+can_solve ops equation =
     case operands equation of
         [] -> False
-        x:xs -> go xs xs $ HashSet.singleton x
-
-{-
-brute_force_possibilities :: [Word -> Word -> Word] -> Word -> [Word] -> [Word]
-brute_force_possibilities _ start [] = [start]
-brute_force_possibilities ops start (x:xs) = do
-    op <- ops
-    brute_force_possibilities ops (op start x) xs
-
--- This brute force actually solves part 2 in a few seconds.
-can_solve_brute_force :: [Word -> Word -> Word] -> Equation -> Bool
-can_solve_brute_force ops (Equation { result, operands }) = elem result
-    $ brute_force_possibilities ops (head operands) (tail operands)
--}
+        x:xs -> HashSet.member x $ work_backwards ops x (result equation) xs
 
 part1 :: Input -> Word
-part1 = sum . map result . filter (can_solve [(+), (*)] [inv_add, inv_mul])
+part1 = sum . map result . filter (can_solve [inv_add, inv_mul])
 
 (.||.) :: Word -> Word -> Word
 x .||. y = read $ show x ++ show y
 
 part2 :: Input -> Word
-part2 = sum . map result . filter (can_solve [(+), (*), cat_words] [inv_add, inv_mul, inv_cat])
+part2 = sum . map result . filter (can_solve [inv_add, inv_mul, inv_cat])
 
 main :: IO ()
 main = do
