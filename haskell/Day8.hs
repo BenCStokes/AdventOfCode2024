405712a087e3fd23917a86831a4dbca009c3ac11
Ben Stokes <ben@benstokes.org.uk>
2024-12-13 00:23:28 +0000

    Day 8 part 2

--- haskell/Day8.hs
+++ haskell/Day8.hs
@@ -14,23 +14,30 @@ parse input =
     let array_bounds = ((0, 0), (height - 1, width - 1)) in
     listArray array_bounds array_contents
 
-part1 :: Map -> Int
-part1 input =
+solve :: (Map -> Point -> Point -> [Point]) -> Map -> Int
+solve antinode_candidates input =
     let antinodes = do
           [pos1, pos2] <- replicateM 2 $ indices input
           guard $ pos1 /= pos2
           let at1 = input ! pos1
           let at2 = input ! pos2
           guard $ at1 /= '.' && at1 == at2
-          let antinode = twice (step $ distance pos1 pos2) pos1
-          guard $ inRange (bounds input) antinode
-          return antinode in
+          takeWhile (inRange $ bounds input) $ antinode_candidates input pos1 pos2 in
     let antinode_at :: UArray Point Bool
         antinode_at = accumArray (\_ _ -> True) False (bounds input) $ map (,()) antinodes in
     foldlArray' (\count present -> if present then count + 1 else count) 0 antinode_at
-  where distance (x1, y1) (x2, y2) = (x2 - x1, y2 - y1)
-        step (x, y) (x0, y0) = (x + x0, y + y0)
-        twice f = f . f
+
+distance :: Point -> Point -> (Int, Int)
+distance (x1, y1) (x2, y2) = (x2 - x1, y2 - y1)
+
+step :: (Int, Int) -> Point -> Point
+step (x, y) (x0, y0) = (x + x0, y + y0)
+
+part1 :: Map -> Int
+part1 = solve $ \input pos1 pos2 -> [step (distance pos1 pos2) pos2]
+
+part2 :: Map -> Int
+part2 = solve $ \input pos1 pos2 -> iterate (step $ distance pos1 pos2) pos1
 
 main :: IO ()
 main = do
@@ -39,4 +46,4 @@ main = do
     putStr "Part 1: "
     print (part1 input)
     putStr "Part 2: "
-    putStrLn "TODO"
+    print (part2 input)
