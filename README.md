85d479ca2fb9060fedab02d34b5ebee80cfa5df7
Ben Stokes <ben@benstokes.org.uk>
2024-12-19 22:17:51 +0000

    Day 6 part 2

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 I'm solving Advent of Code 2024 in Haskell (and maybe other languages; so far I've also done Day 1 in Rust).
 
 # Progress
-Both parts: days 1, 2, 3, 4, 5, 7, 8.
+Both parts: days 1, 2, 3, 4, 5, 6, 7, 8.
 
-Part 1 only: days 6, 9.
+Part 1 only: day 9.
--- haskell/Day6.hs
+++ haskell/Day6.hs
@@ -1,10 +1,11 @@
 import Data.Array.Unboxed
 import qualified Data.HashSet as HashSet
 import Data.HashSet (HashSet)
-import Data.Ix (range)
-import Data.Functor ((<&>))
+import Data.Ix (range, index)
 import Prelude hiding (Left, Right)
 import Data.List (genericLength)
+import Data.Hashable (Hashable, hashWithSalt, hashUsing)
+import Data.Containers.ListUtils (nubIntOn)
 
 type Point = (Word, Word)
 type Map = UArray Point Bool
@@ -22,6 +23,10 @@ parse input =
     (listArray array_bounds array_contents, start_pos)
 
 data Facing = Up | Right | Down | Left
+    deriving (Eq, Enum)
+
+instance Hashable Facing where
+    hashWithSalt = hashUsing fromEnum
 
 step :: Facing -> Point -> Point
 step Up (x, y) = (x - 1, y)
@@ -29,24 +34,43 @@ step Right (x, y) = (x, y + 1)
 step Down (x, y) = (x + 1, y)
 step Left (x, y) = (x, y - 1)
 
+data Path = Loop [Point] | Exit [Point]
+    deriving (Show)
+
+path :: Map -> Point -> Path
+path layout start_pos = go (start_pos, cycle [Up, Right, Down, Left], HashSet.empty, [start_pos])
+  where go (pos, facing, visited, trace)
+          | HashSet.member (pos, head facing) visited = Loop trace
+          | otherwise =
+              let pos' = step (head facing) pos in
+              case layout !? pos' of
+                  Nothing -> Exit trace
+                  Just True -> go (pos, tail facing, visited, trace)
+                  Just False ->
+                      go (pos', facing, HashSet.insert (pos, head facing) visited, pos':trace)
+
 part1 :: (Map, Point) -> Word
 part1 (layout, start_pos) =
-    let start_state =
-          (start_pos, cycle [Up, Right, Down, Left], HashSet.singleton start_pos) in
-    let advance (pos, facing, visited) =
-          let pos' = step (head facing) pos in
-          layout !? pos' <&> \blocked -> if blocked then
-             (pos, tail facing, visited)
-          else
-             (pos', facing, HashSet.insert pos' visited) in
-    let end_state state = case advance state of
-          Nothing -> state
-          Just next -> end_state next in
-    let (_, _, visited) = end_state start_state in
-    fromIntegral $ HashSet.size visited
+    case path layout start_pos of
+        Exit trace -> genericLength $ nubIntOn (index $ bounds layout) trace
+        _ -> error "Guard does not leave map"
+
+part2 :: (Map, Point) -> Word
+part2 (layout, start_pos) = genericLength $ do
+    (obstruction_pos, False) <- assocs layout
+    -- The below line is slow, because (//) is O(size of array). This can be solved e.g. by using
+    -- a mutable vector from the vector package, but it still runs in reasonable time as-is,
+    -- so I'm not going to put effort into optimising further.
+    -- In fact, possible locations for an obstruction which don't actually obstruct the original
+    -- path should also be pruned immediately, so plenty of room for speeding this up.
+    let with_obstruction = layout // [(obstruction_pos, True)]
+    Loop _ <- return $ path with_obstruction start_pos
+    return ()
 
 main = do
     contents <- getContents
     let input = parse contents
     putStr "Part 1: "
     print (part1 input)
+    putStr "Part 2: "
+    print (part2 input)
